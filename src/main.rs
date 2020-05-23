3a14f1e89a95b4227df026c6f001cbb30bed8380
Tomochika Hara <git@thara.jp>
2020-05-23 23:54:52 +0900

    :wrench: Use clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(version = "0.1", author = "Tomochika Hara <bulletlog@thara.dev>")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(name = "add", alias = "a", about = "Add an entry")]
+    Add { note: String },
+}
+
 fn main() {
+    let opts = Opts::parse();
     println!("Hello, world!");
 }
